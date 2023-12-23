24967e0fb4eff7846f034f131fbce8ca1dabad20
Валентин <J.K.play2015@gmail.com>
2023-12-23 17:46:11 +0200

    Initial commit

---
+++ src/test/java/com/secret_santa/secret_santa/SecretSantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secret_santa.secret_santa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretSantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
