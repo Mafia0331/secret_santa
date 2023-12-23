d5b889daaf5e19a9722fae968f619b17ed3f11eb
Валентин <J.K.play2015@gmail.com>
2023-12-23 20:27:08 +0200

    filling botInitializer

--- src/main/java/com/secret_santa/secret_santa/config/BotInitializer.java
+++ src/main/java/com/secret_santa/secret_santa/config/BotInitializer.java
@@ -1,4 +1,26 @@
 package com.secret_santa.secret_santa.config;
 
+import com.secret_santa.secret_santa.service.TelegramBot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
 public class BotInitializer {
+    @Autowired
+    TelegramBot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiException e) {
+            // тут должны быть логи, которые я пропишу чуть позже
+        }
+    }
 }
